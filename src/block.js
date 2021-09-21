dc20212d2ebba4238e066473df9d5f876974b3a2
Julio Murta <souzamurtaj@gmail.com>
2021-09-21 13:06:36 -0300

    fix validate method

--- src/block.js
+++ src/block.js
@@ -39,15 +39,17 @@ class Block {
         let self = this;
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
-            let hash = self.hash;
-                                            
+            let currenthHash = self.hash;
+            self.hash = null;                             
+            
             // Recalculate the hash of the Block
-            let hashToValidate = SHA256(JSON.stringify(this)).toString();  
+            let newHash = SHA256(JSON.stringify(self)).toString();  
+            self.hash = currenthHash;
 
             // Comparing if the hashes changed
             // Returning the Block is not valid            
             // Returning the Block is valid
-            resolve(hash !== hashToValidate);
+            resolve(currenthHash !== newHash);
         });
     }
 
